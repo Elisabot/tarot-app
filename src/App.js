604d5c5d50b968d32fe05415a59a4dc21651b80b
Elisabeth <elisabeth.nimtzmeyer@gmail.com>
2020-03-10 14:03:42 -0700

    wip adjust style and reorganize files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Card } from './components/card';
-import CardGallery from './views/cardGallery';
 
 function App(data) {
   return (
--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// just one card
+const card = {
+  title: 'The World',
+  numeral: 21,
+  arcana: 'greater'
+};
+
+// TODO: pass props to card in the gallery component
+export function Card() {
+  return (
+    <CardContainer className="card-container">
+      <CardBody>
+        <CardText>
+          <p>{card.numeral}</p>
+          <p>{card.title.toUpperCase()}</p>
+          <CardEmoji role="img" aria-label="emoji">
+            🌐
+          </CardEmoji>
+        </CardText>
+      </CardBody>
+    </CardContainer>
+  );
+}
+
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
+  height: 100vh;
 `;
 const CardBody = styled.div`
   width: 350px;
@@ -26,25 +51,3 @@ const CardText = styled.div`
 const CardEmoji = styled.span`
   font-size: 120px;
 `;
-// just one card
-const card = {
-  title: 'The World',
-  numeral: 21,
-  arcana: 'greater'
-};
-
-export function Card(props) {
-  return (
-    <CardContainer className="card-container">
-      <CardBody>
-        <CardText>
-          <p>{card.numeral}</p>
-          <p>{card.title.toUpperCase()}</p>
-          <CardEmoji role="img" aria-label="emoji">
-            🌐
-          </CardEmoji>
-        </CardText>
-      </CardBody>
-    </CardContainer>
-  );
-}
